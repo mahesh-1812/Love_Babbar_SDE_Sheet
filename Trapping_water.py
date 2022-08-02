0607d0dc173c0be0326c9aad0262a9cf1d57d061
Mahesh Kadam <53900864+mahesh-1812@users.noreply.github.com>
2022-08-02 16:25:46 +0530

    Create Trapping_water.py

---
+++ Trapping_water.py
@@ -0,0 +1,25 @@
+#Trapping water
+
+
+def trappingwater(height,n):
+    if not height: 
+        return 0
+    left, right = 0, len(height) - 1
+    leftMax, rightMax = height[left], height[right]
+    result = 0
+       
+    while left < right:
+        if leftMax < rightMax:
+            left += 1
+            leftMax = max(leftMax, height[left])
+            result += leftMax - height[left]
+        else:
+            right -= 1
+            rightMax = max(rightMax, height[right])
+            result += rightMax - height[right]
+    return result
+
+
+height=[3,0,0,2,0,4]
+n=len(height)
+trappingwater(height,n)
